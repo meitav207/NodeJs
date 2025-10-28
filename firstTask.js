706547b097b2e0198a06dc8379bf514cf59b7d1a
meitav207 <meitav1010@gmail.com>
2025-10-28 15:10:02 +0200

    Meitav itah && Ben salomon

---
+++ firstTask.js
@@ -0,0 +1,9 @@
+"use strict";
+//Meitav && Ben
+
+const num = 15;
+const divideBy2 = num % 2 === 0;
+const divideBy3 = num % 3 === 0;
+const divideBy5 = num % 5 === 0;
+
+console.log(Number(divideBy2) + Number(divideBy3) + Number(divideBy5)); //סכימה של המרת ערך בוליאני למספר
